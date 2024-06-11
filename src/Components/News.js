6f66f420b935fc1d2f2f1ebdb5d733a68e4a2dcf
FarzamA-git <89745763+FarzamA-git@users.noreply.github.com>
2024-06-11 17:58:19 +0500

    Update News.js

--- src/Components/News.js
+++ src/Components/News.js
@@ -24,7 +24,7 @@ const News =(props)=>{
   
   useEffect(() => {
       updateNews(page)
-  }, []);
+  }, [page, updateNews]);
 
   const fetchMoreData = async () => {
     const nextPage = page + 1;
